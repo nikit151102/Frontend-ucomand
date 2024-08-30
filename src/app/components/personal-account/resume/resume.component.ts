8978cfa3082c4ca422217432cf5d988f7a99d0de
nikit15110295@gmail.com <60881026+nikit151102@users.noreply.github.com>
2024-08-30 20:02:51 +0700

    .

--- src/app/components/personal-account/resume/resume.component.ts
+++ src/app/components/personal-account/resume/resume.component.ts
@@ -83,7 +83,6 @@ export class PersonalResumeComponent implements OnInit {
       this.resumeService.setArchive(this.cardItem.id, this.cardItem).subscribe(
         (response: any) => {
           console.log("user", response);
-          this.resumeService.updateResumes(this.cardItem.id);
           this.resumeService.updateResumes(this.cardItem)
         });
     }
