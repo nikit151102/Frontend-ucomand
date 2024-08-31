a9752a66ea56b751684149fea2504220008f7315
nikit15110295@gmail.com <60881026+nikit151102@users.noreply.github.com>
2024-08-31 13:21:07 +0700

    .

--- src/app/components/personal-account/personal-data/user-interface.ts
+++ src/app/components/personal-account/personal-data/user-interface.ts
@@ -1,32 +1,32 @@
 export interface District {
     id: number;
-    name: string;
+    name: string | null;
 }
 
 export interface Region {
     id: number;
-    name: string;
-    district: District;
+    name: string | null;
+    district: District | null;
 }
 
 export interface CityOfResidence {
     id: number;
-    name: string;
-    region: Region;
+    name: string | null;
+    region: Region | null;
 }
 
 export interface User {
     id: number;
-    firstName: string;
-    lastName: string;
+    firstName: string | null;
+    lastName: string | null;
     gender: 'MALE' | 'FEMALE'; // Предположим, что возможно два значения
-    age: number;
-    freeLink: string;
-    ownLink: string;
-    aboutMe: string;
-    email: string;
-    telegram: string;
-    dateOfRegistration: string; // Можно использовать Date, если требуется работа с датой
+    age: number | null;
+    freeLink: string | null;
+    ownLink: string | null;
+    aboutMe: string | null;
+    email: string | null;
+    telegram: string | null;
+    dateOfRegistration: string | null; // Можно использовать Date, если требуется работа с датой
     cityOfResidence: CityOfResidence;
     role: 'ROOT' | 'USER' | 'ADMIN'; // Укажите возможные значения для роли
 }
