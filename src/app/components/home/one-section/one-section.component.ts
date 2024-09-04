19c53d3bbe47227fdb5acccf888c00deab535ac6
nikit15110295@gmail.com <60881026+nikit151102@users.noreply.github.com>
2024-09-04 15:31:56 +0700

    .

--- src/app/components/home/one-section/one-section.component.ts
+++ src/app/components/home/one-section/one-section.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, HostListener } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, HostListener, Renderer2 } from '@angular/core';
 import { SearchComponent } from '../search/search.component';
 import { Router } from '@angular/router';
 import { FormSettingService } from '../../form/form-setting.service';
@@ -22,57 +22,43 @@ import { throttle } from 'lodash';
 
 export class OneSectionComponent implements AfterViewInit {
 
-  constructor(public settingHeaderService: SettingHeaderService, private formSettingService: FormSettingService, public tokenService:TokenService, private router: Router, public popUpEntryService: PopUpEntryService) { }
-
-  hasType = false;
+  constructor(public settingHeaderService: SettingHeaderService, 
+    private elRef: ElementRef,  private cdr: ChangeDetectorRef,
+    private renderer: Renderer2,private formSettingService: FormSettingService, public tokenService:TokenService, private router: Router, public popUpEntryService: PopUpEntryService) { }
 
   private stickyOffset: number = 60; 
   private searchElement: HTMLElement | null = null;
-  private fixedPixel: number = 0;
   private isStickyApplied: boolean = false;
 
   ngAfterViewInit() {
-    this.hasType = !!document.querySelector('app-desktop-type') || !!document.querySelector('app-phone-type');
-    this.searchElement = document.querySelector('app-search');
-    this.onWindowScroll(); 
+    this.cdr.detectChanges();
+  this.searchElement = this.elRef.nativeElement.querySelector('app-search');
+  this.startScrollMonitoring();
   }
 
-  
-  handleScroll = throttle(() => {
-    if (this.searchElement) {
-      const scrollTop = window.scrollY || document.documentElement.scrollTop;
-      const elementRect = this.searchElement.getBoundingClientRect();
-      const elementTop = scrollTop + elementRect.top;
-  
-      const shouldApplySticky = scrollTop >= (elementTop - this.stickyOffset) && (scrollTop > this.fixedPixel);
-  
-      if (shouldApplySticky && !this.isStickyApplied) {
-        console.log("add('sticky')");
-        this.searchElement.classList.add('sticky');
-        this.fixedPixel = scrollTop;
-        this.settingHeaderService.isSticky = true;
-        this.isStickyApplied = true;
-      } else if (!shouldApplySticky && this.isStickyApplied) {
-        console.log("remove('sticky');");
-        this.searchElement.classList.remove('sticky');
-        this.fixedPixel = 0;
-        this.settingHeaderService.isSticky = false;
-        this.isStickyApplied = false;
-      }
-    }
-  }, 200);
-  
-  private scrollScheduled: boolean = false;
+  private startScrollMonitoring() {
+    const checkSticky = () => {
+      if (this.searchElement) {
+        const scrollTop = window.scrollY || document.documentElement.scrollTop;
+        const elementRect = this.searchElement.getBoundingClientRect();
+        const elementTop = scrollTop + elementRect.top;
 
-  @HostListener('window:scroll', ['$event'])
-  onWindowScroll() {
-    if (!this.scrollScheduled) {
-      this.scrollScheduled = true;
-      requestAnimationFrame(() => {
-        this.handleScroll();
-        this.scrollScheduled = false; 
-      });
-    }
+        const shouldApplySticky = scrollTop >= (elementTop - this.stickyOffset);
+
+        if (shouldApplySticky && !this.isStickyApplied) {
+          this.renderer.addClass(this.searchElement, 'sticky');
+          this.settingHeaderService.isSticky = true;
+          this.isStickyApplied = true;
+        } else if (!shouldApplySticky && this.isStickyApplied) {
+          this.renderer.removeClass(this.searchElement, 'sticky');
+          this.settingHeaderService.isSticky = false;
+          this.isStickyApplied = false;
+        }
+      }
+      requestAnimationFrame(checkSticky); 
+    };
+
+    requestAnimationFrame(checkSticky);
   }
 
   
