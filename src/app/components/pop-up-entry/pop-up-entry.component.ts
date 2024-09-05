86f264c9a5949d43c1673e190a9595a1f9d898cb
nikit15110295@gmail.com <60881026+nikit151102@users.noreply.github.com>
2024-09-05 11:27:16 +0700

    .

--- src/app/components/pop-up-entry/pop-up-entry.component.ts
+++ src/app/components/pop-up-entry/pop-up-entry.component.ts
@@ -6,6 +6,7 @@ import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
 import { InputTextModule } from 'primeng/inputtext';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-pop-up-entry',
@@ -69,7 +70,7 @@ export class PopUpEntryComponent implements AfterViewInit, OnDestroy, OnInit {
 
   onTelegramAuth(user: any) {
     console.log("Telegram User Data:", user);
-
+  
     // Отправка запроса на ваш сервер для аутентификации
     this.http.post('https://vm-7c43f39f.na4u.ru/api/users/auth/byTelegram', user, {
       headers: { 'Content-Type': 'application/json' }
@@ -77,50 +78,81 @@ export class PopUpEntryComponent implements AfterViewInit, OnDestroy, OnInit {
       console.log("response", response);
       console.log("response.token", response.token);
       this.tokenService.setToken(response.token);
-
-      // Запрос аватарки
+  
+      // Запрос фотографий пользователя
       this.getTelegramUserProfilePhotos(user.id).subscribe(
         (photoData) => {
           console.log('User avatar data:', photoData);
-          const userPhotoUrl = this.extractLargestPhoto(photoData);
-          console.log("userPhotoUrl",userPhotoUrl)
-          // Используйте URL для отображения фото профиля
+          if (photoData.result.total_count > 0) {
+            // Получение file_id самой большой фотографии
+            const largestPhotoFileId = this.extractLargestPhotoFileId(photoData);
+            // Получение фактического URL файла
+            if(largestPhotoFileId)
+            this.getTelegramFileUrl(largestPhotoFileId).subscribe(
+              (fileUrl) => {
+                console.log('User avatar URL:', fileUrl);
+                // Используйте URL для отображения фото профиля
+              },
+              (error) => {
+                console.error('Ошибка при получении URL файла Telegram:', error);
+              }
+            );
+          } else {
+            console.log('Фотографии не найдены для пользователя');
+          }
         },
         (error) => {
-          console.error('Error fetching user avatar:', error);
+          console.error('Ошибка при получении аватарки пользователя:', error);
         }
       );
-
+  
       this.popUpEntryService.getUser().subscribe(
         (data) => {
           this.tokenService.setToken(data.token);
-          console.log('User data:', data.token);
-          this.login_user()
+          console.log('Данные пользователя:', data.token);
+          this.login_user();
         },
         (error) => {
-          console.error('Error fetching user data:', error);
+          console.error('Ошибка при получении данных пользователя:', error);
         }
       );
     });
   }
-
-  // Метод для получения фотографии профиля пользователя Telegram
+  
+  // Запрос фотографий профиля пользователя
   getTelegramUserProfilePhotos(userId: number) {
-    const botToken = 'YOUR_BOT_TOKEN'; // замените на токен вашего бота
-    const apiUrl = `https://api.telegram.org/bot5547226280:AAER7OzBYfDejmrk5BGYBNpwzkPmnDhodTc/getUserProfilePhotos?user_id=${userId}`;
-
-    return this.http.get(apiUrl);
+    const botToken = '5547226280:AAER7OzBYfDejmrk5BGYBNpwzkPmnDhodTc'; // Замените на токен вашего бота
+    const apiUrl = `https://api.telegram.org/bot${botToken}/getUserProfilePhotos?user_id=${userId}`;
+  
+    return this.http.get<any>(apiUrl);
   }
-
-  // Метод для извлечения самой большой фотографии из ответа
-  extractLargestPhoto(photoData: any) {
-    if (photoData && photoData.result && photoData.result.photos && photoData.result.photos.length > 0) {
+  
+  // Извлечение file_id самой большой фотографии
+  extractLargestPhotoFileId(photoData: any): string | null {
+    if (photoData && photoData.result && photoData.result.photos.length > 0) {
       const photos = photoData.result.photos;
-      const largestPhoto = photos[0][photos[0].length - 1]; // Обычно, последняя фотография в массиве является самой большой
-      return `https://api.telegram.org/file/bot<YOUR_BOT_TOKEN>/${largestPhoto.file_path}`;
+      const largestPhoto = photos[0][photos[0].length - 1]; // Обычно последняя фотография в массиве является самой большой
+      return largestPhoto.file_id;
     }
     return null;
   }
+  
+  // Получение URL файла из Telegram с использованием file_id
+  getTelegramFileUrl(fileId: string) {
+    const botToken = 'YOUR_BOT_TOKEN'; // Замените на токен вашего бота
+    const url = `https://api.telegram.org/bot${botToken}/getFile?file_id=${fileId}`;
+  
+    return this.http.get<any>(url).pipe(
+      map((response:any) => {
+        if (response.ok && response.result) {
+          const filePath = response.result.file_path;
+          return `https://api.telegram.org/file/bot${botToken}/${filePath}`;
+        }
+        return null;
+      })
+    );
+  }
+  
   
   login_enter() {
     this.popUpEntryService.visible = false;
