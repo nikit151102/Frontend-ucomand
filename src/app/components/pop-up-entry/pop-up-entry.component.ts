0b426effdded7712ee50d658ea1f05bbafb7648e
nikit15110295@gmail.com <60881026+nikit151102@users.noreply.github.com>
2024-09-08 21:38:23 +0700

    .

--- src/app/components/pop-up-entry/pop-up-entry.component.ts
+++ src/app/components/pop-up-entry/pop-up-entry.component.ts
@@ -50,7 +50,7 @@ export class PopUpEntryComponent implements AfterViewInit, OnDestroy, OnInit {
       script.id = 'telegram-widget-script';
       script.async = true;
       script.src = 'https://telegram.org/js/telegram-widget.js?22';
-      script.setAttribute('data-telegram-login', 'Trireibot');
+      script.setAttribute('data-telegram-login', 'Uteam_top_bot');
       script.setAttribute('data-size', 'large');
       script.setAttribute('data-onauth', 'onTelegramAuth(user)');
       script.setAttribute('data-request-access', 'write');
