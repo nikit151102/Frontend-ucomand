3c29de50af288e9948dec86e07e26eec0620338c
nikit151102 <nikit15110295@gmail.com>
2024-10-28 14:50:14 +0700

    добавлены контролеры в форму создания проекта

--- src/app/components/projects/create-edit-projects/create-edit-projects.component.ts
+++ src/app/components/projects/create-edit-projects/create-edit-projects.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-edit-projects',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './create-edit-projects.component.html',
   styleUrl: './create-edit-projects.component.css'
 })
-export class CreateEditProjectsComponent {
+export class CreateEditProjectsComponent implements OnInit {
+
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+
+  }
+
+  ngOnInit(): void {
+    this.initializeForm()
+  }
+
+  initializeForm(): void {
+    this.form = this.fb.group({
+      nameProject: ['', [Validators.maxLength(100)]],
+      miniDescription: ['', [Validators.required, Validators.maxLength(300)]],
+      typeProject: ['', Validators.required],
+      email: ['', Validators.required],
+      telegram: ['', Validators.required],
+      description: ['', [Validators.required, Validators.maxLength(700)]],
+      stageDevelopment: ['', Validators.required],
+      tasks: ['', Validators.required]
+    });
+  }
+
 
 }
