5aac84b799dbe165caba2759d030484c4b928025
Nikita <nikit15110295@gmail.com>
2025-06-24 22:57:26 +0700

    apiUrl

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     //apiUrl: 'https://uteam.top/api',
-     apiUrl: 'http://dev.uteam.top/api',
+     apiUrl: 'https://dev.uteam.top/api',
     //apiUrl: 'http://localhost:9091/api',
     // userNameBot: 'Uteam_top_bot'
     userNameBot: 'Uteam_top_bot'
