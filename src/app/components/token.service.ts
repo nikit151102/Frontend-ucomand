2008d284dd38f42751ca84e53fa52a57d633076a
Nikita <nikit15110295@gmail.com>
2025-07-01 20:16:21 +0700

    TokenService

--- src/app/components/token.service.ts
+++ src/app/components/token.service.ts
@@ -1,33 +1,41 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class TokenService {
-  private authTokenSubject = new BehaviorSubject<boolean>(this.hasToken());
-  isAuthenticated$ = this.authTokenSubject.asObservable();
+  private authTokenSubject: BehaviorSubject<boolean>;
+  isAuthenticated$: Observable<boolean>;
 
-  constructor() { }
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.authTokenSubject = new BehaviorSubject<boolean>(this.hasToken());
+    this.isAuthenticated$ = this.authTokenSubject.asObservable();
+  }
 
   private hasToken(): boolean {
-    return !!localStorage.getItem('authToken');
+    if (isPlatformBrowser(this.platformId)) {
+      return !!localStorage.getItem('authToken');
+    }
+    return false;
   }
 
   getToken(): boolean {
     return this.authTokenSubject.value;
   }
 
-  setToken(token:string): void {
-    localStorage.setItem('authToken', token);
+  setToken(token: string): void {
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('authToken', token);
+    }
     this.authTokenSubject.next(true);
   }
 
   clearToken(): void {
-    localStorage.removeItem('authToken');
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.removeItem('authToken');
+    }
     this.authTokenSubject.next(false);
   }
-  
-
 }
