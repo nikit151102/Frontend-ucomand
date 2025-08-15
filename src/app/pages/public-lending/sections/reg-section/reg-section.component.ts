aaa5a37b062e826c8cc598badfac8cce3f227955
mossoii84 <mikaproger@gmail.com>
2025-08-15 22:17:58 +0200

    two page

---
+++ src/app/pages/public-lending/sections/reg-section/reg-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reg-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './reg-section.component.html',
+  styleUrl: './reg-section.component.css'
+})
+export class RegSectionComponent {
+
+}
