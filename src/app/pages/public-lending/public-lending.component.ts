a809666a22425d1b7fcbc0da4f8a534e234dd804
mossoii84 <mikaproger@gmail.com>
2025-09-18 20:43:34 +0200

    seventh-section

--- src/app/pages/public-lending/public-lending.component.ts
+++ src/app/pages/public-lending/public-lending.component.ts
@@ -5,12 +5,14 @@ import { FourthSectionComponent } from "./sections/fourth-section/fourth-section
 import { FifthSectionComponent } from "./sections/fifth-section/fifth-section.component";
 import { TwoSectionComponent } from './sections/two-section/two-section.component';
 import { SixthSectionComponent } from "./sections/sixth-section/sixth-section.component";
+import { SeventhSectionComponent } from "./sections/seventh-section/seventh-section.component";
+import { LendingFooterButtonComponent } from "./sections/lending-footer-button/lending-footer-button.component";
 
 @Component({
   selector: 'app-public-lending',
   standalone: true,
-  imports: [OneSectionComponent, TwoSectionComponent, ThirdSectionComponent, 
-    FourthSectionComponent, FifthSectionComponent, SixthSectionComponent],
+  imports: [OneSectionComponent, TwoSectionComponent, ThirdSectionComponent,
+    FourthSectionComponent, FifthSectionComponent, SixthSectionComponent, SeventhSectionComponent],
   templateUrl: './public-lending.component.html',
   styleUrl: './public-lending.component.css'
 })
